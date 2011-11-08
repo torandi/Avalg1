53584ed45984ba48b8f24d54434361a06eac2735
Andreas Tarandi <taran@kth.se>
2011-11-08 13:58:56 +0100

    Adds pollard

--- factoring.cpp
+++ factoring.cpp
@@ -9,11 +9,14 @@ using namespace std;
 #define PRIME_TEST_REPS 10
 
 void init();
-bool factorize(const mpz_class &num, list<mpz_class> &factors);
+bool factorize(mpz_t num, list<mpz_class> &factors);
 void output_factors(list<mpz_class> &factors);
+void pollard(mpz_t ret, mpz_t N, mpz_t x0);
+void square_plus_one(mpz_t &z);
 
 int main() {
-   mpz_class num;
+   mpz_t num;
+   mpz_init(num);
    init();
 
    list<mpz_class> factors;
@@ -21,7 +24,6 @@ int main() {
     while(!cin.eof()) {
       cin>>num;
       if(!cin.eof()) {
-         cerr<<"Num: "<<num<<endl;
          if(factorize(num,factors)) {
             output_factors(factors);
          } else {
@@ -43,15 +45,54 @@ void output_factors(list<mpz_class> &factors) {
    }
 }
 
-bool factorize(const mpz_class &num, list<mpz_class> &factors) {
-   int prob_prime = mpz_probab_prime_p(num.get_mpz_t(), PRIME_TEST_REPS);
+bool factorize(mpz_t num, list<mpz_class> &factors) {
+   int prob_prime = mpz_probab_prime_p(num, PRIME_TEST_REPS);
    if(prob_prime == 0) {
-      //Do polarts
-      return false;
+      mpz_t d, tmp, x0;
+      mpz_init(d);
+      mpz_init(tmp);
+      mpz_init(x0);
+      mpz_set_ui(x0, 2);
+      pollard(d, num, x0);
+      mpz_div(tmp, num, d);
+      if(factorize(d, factors) && factorize(tmp, factors))
+         return true;
+      else
+         return false;
    } else {
       if(prob_prime == 1)
          cerr<<num<<" is probably prime"<<endl;
-      factors.push_back(num);
+      factors.push_back(mpz_class(num));
       return true;
    }
+}
+
+void pollard(mpz_t d, mpz_t N, mpz_t x0) {
+   mpz_t x1, x2, tmp;
+   mpz_init(x1);
+   mpz_init(x2);
+   mpz_init(d);
+
+   mpz_set(x1, x0);
+   mpz_set(x2, x0);
+   mpz_set_ui(d, 1);
+
+   while(mpz_cmp_ui(d,1)) {
+      square_plus_one(x1);
+      mpz_mod(x1, x1, N);
+
+      square_plus_one(x2);
+      square_plus_one(x2);
+      mpz_mod(x2, x2, N);
+
+      mpz_sub(tmp, x2, x1);
+      mpz_abs(tmp, tmp);
+
+      mpz_gcd(d, tmp, N);
+   }
+}
+
+void square_plus_one(mpz_t &z) {
+   mpz_pow_ui(z, z , 2);
+   mpz_add_ui(z ,z ,1);
 }
