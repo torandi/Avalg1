2bd7c66585e0ee3f95cd3dac85ec80233152f8b5
Andreas Tarandi <taran@kth.se>
2011-11-08 15:09:47 +0100

    Pollard returns fail on failure

--- factoring.cpp
+++ factoring.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 void init();
 bool factorize(mpz_t num);
-void pollard(mpz_t ret, mpz_t N, mpz_t x0);
+bool pollard(mpz_t ret, mpz_t N, mpz_t x0);
 void square_plus_one(mpz_t &z);
 
 int main() {
@@ -47,28 +47,31 @@ bool factorize(mpz_t num) {
       mpz_init(tmp);
       mpz_init(x0);
       mpz_set_ui(x0, 2);
-      pollard(d, num, x0);
-      mpz_fdiv_q(tmp, num, d);
-      cerr<<"Pollard: "<<d<<", "<<tmp<<endl;
-      if(factorize(d) && factorize(tmp))
-         return true;
-      else
+      if(pollard(d, num, x0)) {
+         mpz_fdiv_q(tmp, num, d);
+         cerr<<"Pollard: "<<d<<", "<<tmp<<endl;
+         if(factorize(d) && factorize(tmp))
+            return true;
+         else
+            return false;
+      } else {
+         cerr<<"Pollard failed!"<<endl;
          return false;
+      }
    } else {
       if(prob_prime == 1)
          cerr<<num<<" is probably prime"<<endl;
-      else
-         cerr<<num<<" is definitly prime"<<endl;
+      /*else
+         cerr<<num<<" is definitly prime"<<endl;*/
       cout<<num<<endl;
       return true;
    }
 }
 
-void pollard(mpz_t d, mpz_t N, mpz_t x0) {
+bool pollard(mpz_t d, mpz_t N, mpz_t x0) {
    mpz_t x1, x2, tmp;
    mpz_init(x1);
    mpz_init(x2);
-   mpz_init(d);
    mpz_init(tmp);
 
    mpz_set(x1, x0);
@@ -83,11 +86,14 @@ void pollard(mpz_t d, mpz_t N, mpz_t x0) {
       square_plus_one(x2);
       mpz_mod(x2, x2, N);
 
+      cout<<"x1: "<<x1<<", x2: "<<x2<<endl;
+
       mpz_sub(tmp, x2, x1);
       mpz_abs(tmp, tmp);
 
       mpz_gcd(d, tmp, N);
    }
+   return !(mpz_cmp(d,N)==0);
 }
 
 void square_plus_one(mpz_t &z) {
